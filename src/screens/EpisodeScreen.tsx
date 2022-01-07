0fb0c6b393cb1200d21f8306eabb5516363e021f
tehdevv <andredev.santos@gmail.com>
2022-01-07 22:45:03 +0000

    Update EpisodeScreen.tsx to display episode info

--- src/screens/EpisodeScreen.tsx
+++ src/screens/EpisodeScreen.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import { useLogging } from '../hooks/useLogging';
 import { IStackScreenProps } from '../library/StackScreenProps';
+import { useState } from 'react';
+
+export interface IEpisode{
+    name: string,
+    image: {
+        original: string,
+    }
+    season: string,
+    runtime: number,
+    airdate: string,
+    summary: string
+}
 
 const EpisodeScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
-    const [logging] = useLogging('Contact Screen');
+    const [logging] = useLogging('Episode Screen');
     const { navigation, route } = props;
+    const [epData, setEpData] = useState();
+
+    navigation.setOptions({ title: `Episode ${route.params.number} from season ${route.params.season}` });
 
     useEffect(() => {
         logging.info({ navigation, route });
@@ -14,9 +29,15 @@ const EpisodeScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>Episode screen</Text>
-            <Button title="Home" onPress={() => navigation.navigate('Home')} />
-            <StatusBar style="auto" />
+            <Image
+            style={{width: '100%', aspectRatio: 3/2}}
+                source={{ uri: route.params.image.original }}
+            />
+            <Text style={styles.title}>{route.params.name}</Text>
+            <Text style={styles.textDetails}>Episode {route.params.number} from season {route.params.season}</Text>
+            <Text style={styles.textDetails}>Runtime: {route.params.runtime}</Text>
+            <Text style={styles.textDetails}>Airdate: {route.params.airdate}</Text>
+            <Text>{route.params.summary}</Text>
         </View>
     );
 };
@@ -24,8 +45,24 @@ const EpisodeScreen: React.FunctionComponent<IStackScreenProps> = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        padding: 30
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginVertical: 6
+    },
+    subTitle: {
+        fontSize: 20,
+    },
+    textDetailsSmall: {
+        fontSize: 13,
+        marginVertical: 6
+    },
+    summary: {
+        fontSize: 18
     }
 });
 
