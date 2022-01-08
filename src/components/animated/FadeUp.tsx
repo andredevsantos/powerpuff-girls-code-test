cd1a5ada275caee19d660e5434c52daa29102971
tehdevv <andredev.santos@gmail.com>
2022-01-08 01:43:22 +0000

    Add FadeUp animated component

---
+++ src/components/animated/FadeUp.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from "react"
+import { Animated, View } from "react-native"
+
+export type Props = {
+    children: any,
+    delay?: number,
+    duration?: number | 10000
+}
+
+const FadeUp = (props: Props) => {
+    const opacity = useRef(new Animated.Value(0)).current
+    const position = useRef(new Animated.Value(100)).current
+
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(opacity, {
+                toValue: 1,
+                duration: props.duration,
+                delay: props.delay,
+                useNativeDriver: true
+            }),
+            Animated.timing(position, {
+                toValue: 0,
+                duration: props.duration,
+                delay: props.delay,
+                useNativeDriver: true
+            })
+        ]).start()
+    }, [opacity])
+
+
+    return (
+        <Animated.View style={{ opacity: opacity, transform: [{ translateY: position }] }}>{props.children}</Animated.View>
+    )
+}
+
+export default FadeUp
